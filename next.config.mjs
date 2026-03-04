008af96e1efa27d25082d467af5c8521e1d72b32
Bhoomi Gupta <bhoomigupta0626@gmail.com>
2026-03-04 21:35:39 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  // images: {
-  //   domains: ["randomuser.me", "images.unsplash.com"],
-  // },
-
-   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "example.com",
-      },
-    ],
+  images: {
+    domains: ["randomuser.me", "images.unsplash.com"],
   },
-
+  
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
